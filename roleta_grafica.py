b1392c7403a375b6252b0ca200738c4aefb89f85
judismar <solid.judis@gmail.com>
2023-08-24 11:52:23 -0300

    Com atualizações e menos redundância de código.

--- roleta_grafica.py
+++ roleta_grafica.py
@@ -1,36 +1,44 @@
 from random import randint
 from tkinter import *
+from functools import partial
+
+#Crédito aos alunos Pablo, Luiz Felipe e Serra pela ideia do jogo da roleta. :)
+#Usem este código como base para desenvolver outros apps com tkinter (para quem gostou).
 
 #A dinâmica do jogo:
-def escolheuVermelho():
-    res = randint(0, 1)
-    if res == 0:
-        resultado.set("Você venceu!")
-        x = float(aposta.get())
-        y = float(saldo.get())
-        y += 2*x
-        saldo.set(str(y))
-    else:
-        resultado.set("Você perdeu!")
-        x = float(aposta.get())
-        y = float(saldo.get())
-        y -= x
-        saldo.set(str(y))
-        
-def escolheuPreto():
-    res = randint(0, 1)
-    if res == 1:
-        resultado.set("Você venceu!")
-        x = float(aposta.get())
-        y = float(saldo.get())
-        y += 2*x
-        saldo.set(str(y))
-    else:
-        resultado.set("Você perdeu!")
-        x = float(aposta.get())
-        y = float(saldo.get())
-        y -= x
-        saldo.set(str(y))
+def pretoOuVermelho(cor, aposta, saldo, resultado):
+	res = randint(0, 1)
+	valorAposta = float(aposta.get())
+	valorSaldo = float(saldo.get())
+	if valorAposta > valorSaldo:
+		resultadoRotulo["foreground"] = "red"
+		resultado.set("O valor da aposta não pode ser\nmaior do que o saldo atual.")
+		return
+	if not valorAposta >= 0:
+		resultadoRotulo["foreground"] = "red"
+		resultado.set("O valor da aposta não pode ser\nmenor que 0.")
+		return
+	resultadoRotulo["foreground"] = "black"
+	if res == 0:
+		s = "Caiu na casa preta.\n"
+		if cor == "preta":
+			resultado.set(s + "Você venceu!")
+			strSaldo["foreground"] = "green"
+			saldo.set(str(valorSaldo + 2*valorAposta))
+		else:
+			resultado.set(s + "Você perdeu!")
+			strSaldo["foreground"] = "red"
+			saldo.set(str(valorSaldo - valorAposta))
+	else:
+		s = "Caiu na casa vermelha.\n"
+		if cor == "vermelha":
+			resultado.set(s + "Você venceu!")
+			strSaldo["foreground"] = "green"
+			saldo.set(str(valorSaldo + 2*valorAposta))
+		else:
+			resultado.set(s + "Você perdeu!")
+			strSaldo["foreground"] = "red"
+			saldo.set(str(valorSaldo - valorAposta))
 
 #A parte gráfica:
 r = Tk()
@@ -38,11 +46,12 @@ saldo = StringVar()
 saldo.set("5000.00")
 resultado = StringVar()
 aposta = StringVar()
+aposta.set("0")
 
 strSaldo = Label(textvariable=saldo, font="Helvetica, 32")
 perguntaLabel = Label(text="Escolha entre vermelho ou preto.", font="Helvetica, 32")
-vermelho = Button(text="Vermelho", command=escolheuVermelho, font="Helvetica, 20")
-preto = Button(text="Preto", command=escolheuPreto, font="Helvetica, 20")
+vermelho = Button(text="Vermelho", command=partial(pretoOuVermelho, "vermelha", aposta, saldo, resultado), font="Helvetica, 20")
+preto = Button(text="Preto", command=partial(pretoOuVermelho, "preta", aposta, saldo, resultado), font="Helvetica, 20")
 apostaEntrada = Entry(textvariable=aposta, font="Helvetica, 20")
 resultadoRotulo = Label(textvariable=resultado, font="Helvetica, 32")
 
@@ -52,3 +61,5 @@ vermelho.pack()
 preto.pack()
 apostaEntrada.pack()
 resultadoRotulo.pack()
+
+r.mainloop()
