788e3ae9fcf4be68a2f1bc40219cb1d2520c5707
judismar <solid.judis@gmail.com>
2023-11-10 15:59:21 -0300

    Delete arquivo.py

--- arquivo.py
+++
@@ -1,9 +0,0 @@
-textoImpresso = ""
-
-  nome = recuperaValorArquivo("nome.txt")
-  nota1 = recuperaValorArquivo("nota-cert-1.txt")
-  nota2 = recuperaValorArquivo("nota-cert-1.txt")
-  nota3 = recuperaValorArquivo("nota-cert-1.txt")
-  
-textoImpresso += "Nota de " += nome += float(int(nota1 + nota2 + nota3)/3))
-print(textoImpresso))
